83139639231e425e75c5c955e37136ff8ed45196
Saieshen Bhagwandin <SaieshenB@hollywoodbets.net>
2023-02-14 08:58:05 +0200

    Add project files.

---
+++ DapperProject/Classes/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperProject.Classes
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+    }
+}
